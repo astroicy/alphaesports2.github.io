15fe01ec15788591deac6d3d4410a5684bc89fea
GxoldXNote <coldfrezzeop@gmail.com>
2022-05-14 15:58:57 +0200

    Alpha Esports

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("btn").onclick = function () {
+  window.location.href = "https://discord.gg/prgCyjRb";
+};
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
